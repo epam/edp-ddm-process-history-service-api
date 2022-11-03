8a33f9efc109ffa97450b3b8ab3c5ed5dc6d4fdd
Oleksii Ponochevnyi <oleksii_ponochevnyi@epam.com>
2022-11-03 19:29:45 +0200

    [EDP-DDM-20815] Added desc sorting by 'startTime' when requested sorting by 'statusTitle'
    Change-Id: I4891f7cc1483fc411253ee70778f564b6806d06f

--- src/main/java/com/epam/digital/data/platform/bphistory/service/api/repository/entity/UpdaptableBpmHistoryProcess.java
+++ src/main/java/com/epam/digital/data/platform/bphistory/service/api/repository/entity/UpdaptableBpmHistoryProcess.java
@@ -17,8 +17,6 @@
 package com.epam.digital.data.platform.bphistory.service.api.repository.entity;
 
 import lombok.Data;
-import org.hibernate.annotations.Immutable;
-import org.hibernate.annotations.Subselect;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
